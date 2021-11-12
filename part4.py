3a66c0745aa0c08fa1a3162a95e7e68ba6854022
Mostaalba8 <mostafa.albakaa@a115.bg>
2021-11-12 09:14:11 +0000

    Part 4 refactored exercises

--- part4.py
+++ part4.py
@@ -19,19 +19,20 @@
 #
 # for letter in "Mostafa":
 #     print(letter)
-
-list_of_powers = {'rasengan':80, 'shadow clone':80, 'fireball':50, 'waterstyle':40}
+list_of_powers = {'rasengan': 80, 'shadow clone': 80, 'fireball': 50, 'waterstyle': 40}
 print(list_of_powers)
-wallet = {'GBP':1023, 'USD':130, 'EUR':320, 'BTC':0.02543222}
+wallet = {'GBP': 1023, 'USD': 130, 'EUR': 320, 'BTC': 0.02543222}
 print(wallet)
 print('')
 
 
 def main():
     power_level_above(list_of_powers, 50)
-    spend_from_wallet(wallet, 'gBp', 23)
-    spend_from_wallet(wallet, 'bTc', 0.02)
-    spend_from_wallet(wallet, 'USd', 2003)
+    spend_from_wallet(wallet, 'GBP', 10)
+    spend_from_wallet(wallet, 'bTc', 0.4)
+    spend_from_wallet(wallet, 'USd', 30)
+    spend_from_wallet(wallet, 'udS', 2003)
+
 
 def power_level_above(list, num):
     temp_list = []
@@ -40,47 +41,15 @@ def power_level_above(list, num):
             temp_list.append(i)
     print(temp_list)
     print('')
+    return temp_list
 
 
-def spend_from_wallet(wallet_name, currency_name, amount):
-
-    for i in wallet_name:
-        if i.lower() == currency_name.lower():
-            if wallet_name[i] >= amount:
-                wallet_name[i] = wallet_name[i] - amount
-                print('')
-                print('Spending ' + i)
-                print('Spent:')
-                print(amount)
-                print('Amount Left:')
-                print(wallet_name[i])
-                print('')
-                break
-            else:
-                print('')
-                print('Trying to spend:', amount)
-                print('Insufficient amount in wallet for currency: ' + i)
-                print('Amount left: ')
-                print(wallet_name[i])
-                print('')
-                break
-        elif wallet_name[i] != i:
-            continue
-
-        elif len(i) == len(wallet_name):
-            print('')
-            print('Currency ' + '"' + currency_name.lower() + '"' + ' does not exist in your wallet.')
-            print('Currencies in wallet: ')
-            print(wallet_name.keys())
-            continue
-
-    print('')
-    print('Overall wallet after spendings: ')
-    print(wallet_name)
-
-
-
-
+def spend_from_wallet(wallet, currency, amount):
+    currency = currency.upper()
+    can_spend = min(amount, wallet.get(currency, 0))
+    wallet[currency] = wallet.get(currency, 0) - can_spend
+    print(can_spend)
+    return can_spend
 
 
 main()
