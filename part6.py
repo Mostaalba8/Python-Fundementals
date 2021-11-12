7e9697e47a9fcd65d4cdc1d985e0db9bceb2e8af
Mostaalba8 <mostafa.albakaa@a115.bg>
2021-11-12 14:09:58 +0000

    Part 6 - Answering questions

---
+++ part6.py
@@ -0,0 +1,18 @@
+## What is “tech debt”? Why is that a thing? What are some ways of managing it?
+# Tech debt is when the developer is looking for the simplest code that serves its purpose.
+# Tech debt is a thing as it can be useful for short term solutions and quick results but will cause a lot of trouble
+# and money for long term projects.
+# To avoid/minimize tech debt and future trouble, the company will need to:
+# - understand market requirements
+# - understand decision consequences
+# - enough supervision throughout the process
+
+## What is “spaghetti code”?
+# The term 'spaghetti code' is used to describe a piece of code that jumps all over the place to complete its purpose.
+
+## What is “cruft”?
+# 'cruft' is when a code/program is badly designed by being over-complicated or when the code/program is unwanted.
+
+## What is “refactoring”?
+# 'refactoring' is when you re-design a piece of code yet keeping the same outcome/purpose. The purpose of refactoring'
+# is to make the code more understandable and efficient.
