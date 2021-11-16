9bd1866c3c3859f1e61554d1bec8e1317af3e2e5
Mostaalba8 <mostafa.albakaa@a115.bg>
2021-11-16 10:13:12 +0000

    minor changes

--- part7.py
+++ part7.py
@@ -7,15 +7,15 @@
 #             print(f'---y = {temp_y}')
 #             if i != y:
 #                 if i + y == target:
-#                     temp_tuple = (temp_i,temp_y)
+#                     temp_tuple = (temp_i, temp_y)
 #                     return temp_tuple
-#                     break
 #             temp_y += 1
 #         temp_y = 0
 #         temp_i += 1
 
+
 # refactored find_target
-def find_target(nums: int, target: int) -> tuple:
+def finding_target(nums: int, target: int) -> tuple:
     for first_index, first_value in enumerate(nums):
         for second_index, second_value in enumerate(nums):
             if first_value + second_value == target:
@@ -23,5 +23,7 @@ def find_target(nums: int, target: int) -> tuple:
                 return temp_tuple
 
 
-nums = (2, 7, 11, 15)
-print(find_target(nums, 18))
+
+
+nums = (2, 7, 11, 16)
+print(find_target(nums, 1800))
