db4e29f819aec7cd1170e4c5ac54d3e73387f12f
Mostaalba8 <mostafa.albakaa@a115.bg>
2021-11-16 11:01:42 +0000

    minor changes

--- part7.py
+++ part7.py
@@ -13,16 +13,16 @@
 #         temp_y = 0
 #         temp_i += 1
 
-from typing import List
 
 
 # refactored find_target
 def finding_target(nums: int, target: int) -> tuple:
-    if type(target) is not (int or float):
-        raise TypeError
+    # if type(target) is not (int or float):
+    #     raise TypeError
 
-    if type(nums) is not List[int]:
-        raise TypeError
+    # for i in nums:
+    #     if type(i) is not (int or float):
+    #         raise TypeError
 
     result_tuple = None
     for first_index, first_value in enumerate(nums):
@@ -35,9 +35,10 @@ def finding_target(nums: int, target: int) -> tuple:
         return 'Not found'
 
 
+
 nums = (2, 7, 11, 16)
 
-if finding_target(nums, 18) in [(0, 3), (1, 2), (2, 1)]:
+if finding_target(nums, 18) in [(0,3), (1,2), (2,1)]:
     print(True)
 
 print(finding_target(nums, 18))
